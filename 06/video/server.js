30f3f75baa54eb89ad2fc302d90f809d6a490acf
wangjiaqi <521128056@qq.com>
2023-01-03 21:47:54 +0800

    feat: 完成视频的简单范围请求

--- 06/video/server.js
+++ 06/video/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const { URL } = require("url");
 const { Buffer } = require("node:buffer");
 
-const hostname = "127.0.0.1";
+const hostname = "www.zaking.com";
 const port = 9000;
 
 const server = http.createServer((req, res) => {
@@ -13,6 +13,11 @@ const server = http.createServer((req, res) => {
     res.writeHead(200);
     res.end();
     return;
+  }
+  if (parsedUrl.pathname == "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end("Please Try /home");
   }
   if (parsedUrl.pathname == "/home") {
     let sourceCode = fs.readFileSync(
@@ -42,7 +47,6 @@ const server = http.createServer((req, res) => {
     for (let i = 0; i < bufSource.length; i += chunkSize) {
       chunks.push(Uint8Array.prototype.slice.call(bufSource, i, i + chunkSize));
     }
-    console.log(chunks, "chunks");
     for (let i = 0; i < chunks.length; i++) {
       const chunk = chunks[i];
       res.write(chunk);
@@ -56,6 +60,36 @@ const server = http.createServer((req, res) => {
       res.write(" World");
       res.end();
     }, 1000);
+  }
+  if (parsedUrl.pathname == "/simple-range") {
+    let videoSource = fs.readFileSync(
+      path.resolve(__dirname, "../media/maomao.mp4")
+    );
+    // 转换
+    const bufSource = Buffer.from(videoSource);
+    // 获取长度
+    const bufSourceLen = bufSource.length;
+    // 获取请求的Range头的长度范围
+    const range = req.headers["range"];
+    const rangeVal = range.split("=")[1].split("-");
+    // 获取开始和结束的长度
+    const start = parseInt(rangeVal[0], 10);
+    const end = rangeVal[1] ? parseInt(rangeVal[1], 10) : start + bufSourceLen;
+    console.log(start, end, bufSourceLen);
+    // 判断是否超出请求资源的最大长度,就返回416
+    if (start > bufSourceLen || end > bufSourceLen) {
+      res.writeHead(416, { "Content-Range": `bytes */${bufSourceLen}` });
+      res.end();
+    } else {
+      // 否则返回206即可
+      res.writeHead(206, {
+        "Content-Range": `bytes ${start}-${end}/${bufSourceLen}`,
+        "Accept-Ranges": "bytes",
+        "Content-type": "video/mp4",
+      });
+      res.write(Uint8Array.prototype.slice.call(bufSource, start, end));
+      res.end();
+    }
   }
 });
 
