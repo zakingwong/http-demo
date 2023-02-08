326bbf0d3cef8a05e3842a9291a3b5c0ebe2695d
wangjiaqi <521128056@qq.com>
2023-02-08 21:30:22 +0800

    feat: 新增缓存例子

---
+++ 10/server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const { URL } = require("url");
+const hostname = "127.0.0.1";
+const port = 9001;
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = new URL(req.url, "http://www.zaking.com");
+
+  if (parsedUrl.pathname == "/cache") {
+    let sourceCode = fs.readFileSync(
+      path.resolve(__dirname, "./index.html"),
+      "utf8"
+    );
+    res.setHeader("Cache-Control", "must-revalidate");
+    res.end(sourceCode);
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
